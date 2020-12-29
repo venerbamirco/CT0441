4a1bd524eea86952634e0f6de89773139a373f93
venerbamirco <venerbamirco@mail.com>
2020-12-29 21:31:29 +0100

    Caricamento

---
+++ Impl.fs
@@ -0,0 +1,276 @@
+﻿
+(*
+ * SharpSolver - Progetto di Programmazione e Calcolo a.a. 2018-19
+ * Impl.fsi: implementazioni degli studenti
+ * (C) 2018 Alvise Spano' @ Universita' Ca' Foscari di Venezia
+ *)
+
+module SharpSolver.Impl
+
+open Absyn
+open Prelude
+open System
+
+//funzione che dato un numero con la virgola lo trasforma in un numero senza virgola e diviso per potenza di dieci
+let rationalize ( x : float ) : rational = 
+    //ho usato questo modo per risolvere problemi di arrotondamento nei casi 7.343 e 0.235999
+    //estraggo la lunghezza del float
+    let cont = float ( x.ToString().Length )
+    //creo il numeratore
+    let num = ( int ) ( x * 10.0 ** cont )
+    //creo il denominatore
+    let den = ( int ) ( 10.0 ** cont )
+    //creo il rational e lo restituisco
+    rational ( num , den )
+    
+//funzione che mi restituisce il grado del monomio
+let monomial_degree ( m : monomial ) : int = 
+    //serve per estrarre la coppia coefficiente grado
+    match m with
+    | Monomial ( coefficiente , grado ) -> grado
+
+//funzione che nega il coefficiente
+let monomial_negate ( m : monomial ) : monomial = 
+    match m with
+    //serve per estrarre la coppia coefficiente grado
+    | Monomial ( coefficiente , grado ) -> 
+        //restituisco il coeffiente negato
+        Monomial ( - coefficiente , grado )
+
+//funzione che restituisce il grado del polinomio
+let polynomial_degree ( p : polynomial ) : int = 
+
+    //funzione ausiliaria
+    let aux ( p : polynomial ) ( acc : int ) : int =
+
+        //funzione che data una lista di monomial mi resituisce il grado piu alto
+        let rec grado_maggiore ( lista : monomial list ) ( gradomax : int ) : int =
+            //scorro la lista
+            match lista with
+            //restituisco il grado maggiore
+            | [] -> gradomax
+            //guardo se quelli successivi sono piu grandi
+            | x :: xs ->
+                //se il grado del monomio è maggiore di quello memorizzato allora lo assegno
+                if monomial_degree x > gradomax then
+                    grado_maggiore xs ( monomial_degree x )
+                //oppure passo quello già memorizzato perche maggiore
+                else grado_maggiore xs gradomax
+
+        //fa parte della funzione aux
+        in
+        match p with
+        //estraggo la lista di monomi che compongono polynomial
+        | Polynomial ( xs ) -> 
+            //calcolo il grado piu elevato tra tutti i monomi
+            grado_maggiore xs 0
+
+    //fa parte della funzione polynomial_degree
+    in 
+    aux p 0
+
+//funzione che restituisce il polinomio negato
+let polynomial_negate ( p : polynomial ) : polynomial = 
+
+    //funzione per negare coefficienti lista monomial
+    let rec nega_coefficienti ( lista : monomial list ) ( acc : monomial list ) : monomial list =
+        //scorro la lista
+        match lista with
+        //restituisco la lista di monomi negata
+        | [] -> acc
+        //vado a negare i monomi successivi
+        | x :: xs ->
+            match x with
+            //estraggo il coefficiente da monomial
+            | Monomial ( coefficiente , grado ) ->
+                //uso un parametro accumulatore per concatenare il singolo monomio negato
+                nega_coefficienti xs ( acc @ [ Monomial ( - coefficiente , grado ) ] )
+
+    //fa parte della funzione polynomial_negate
+    in
+    //estraggo la lista di monomial che compongono polinomial
+    match p with
+    | Polynomial ( xs ) ->
+        //trasformo il monomial list in polinomial
+        Polynomial ( nega_coefficienti xs [] )
+
+//prende un polinomio normalizzato e restituisce il suo grado
+let normalized_polynomial_degree (np : normalized_polynomial) : int = 
+    //estraggo l array di rational che lo compone
+    match np with
+    | NormalizedPolynomial ( xs ) ->
+        //dato che è normalizzato quindi non ci sono monomi simili, allora il grado è la lunghezza meno 1
+        Array.length xs - 1
+    
+//funzione che mi normalizza un polinomio
+let normalize ( p : polynomial ) : normalized_polynomial = 
+
+    //funzione che data una lista di monomi somma quelli simili
+    let rec calcola_monomi_simili ( lista : monomial list ) ( acc : rational [] ) : rational [] =
+
+        //funzione che mi restituisce la lista dopo aver inserito il coefficiente del monomio in posizione del grado
+        let inserisci ( coefficiente : rational ) ( posizione : int ) ( array : rational [] ) : rational [] =
+            //sommo il valore nuovo al valore precedente
+            array.[ posizione ] <- array.[ posizione ] + coefficiente
+            array
+
+        //fa parte della funzione calcola_monomi_simili
+        in
+        //scorro la lista
+        match lista with
+        //restituisco il polinomio normalizzato dopo aver sommato tutti i monomi simili
+        | [] -> acc
+        //guardo se ci sono altri monomi simili
+        | x :: xs ->
+            //estraggo il singolo monomio come coppia coefficiente grado
+            match x with
+            | Monomial ( coefficiente , grado ) ->
+                //richiamo la funzione subito dopo aver sommato nella giusta posizione della lista il singolo monomio selezionato
+                calcola_monomi_simili xs ( inserisci coefficiente grado acc )
+                
+    //fa parte della funzione normalize
+    in
+    //estraggo la lista di monomi
+    match p with
+    | Polynomial ( xs ) ->
+        //determino la lunghezza della lista di monomi
+        let lunghezzapol = polynomial_degree ( Polynomial ( xs ) )
+        //creo array di n posizioni
+        let array = Array.init ( lunghezzapol + 1 ) ( fun _ -> rational ( 0 , 1 ) )
+        //restituisco il polinomio normalizzato
+        NormalizedPolynomial ( calcola_monomi_simili xs array )
+       
+//funzione che deriva un polinomio
+let derive (p : polynomial) : polynomial =
+
+    //funzione per derivare singolo monomio
+    let deriva_singolo_monomio ( monomio : monomial ) : monomial =
+        //estraggo coefficiente e grado
+        match monomio with
+        | Monomial ( coefficiente , grado ) ->
+            //se il grado è uguale a 0
+            if grado = 0 then Monomial ( rational ( 0 , 1 ) , 0 )
+            //uso la formula di derivazione di un polinomio n*ax^(n-1)
+            else
+            Monomial ( ( rational ( grado ) ) * coefficiente , grado - 1 )
+
+    //funzione ausiliare
+    let rec aux ( lista : monomial list ) ( acc  : monomial list ) : monomial list =
+        //scorro la lista
+        match lista with
+        //restituisco la lista di monomi derivati
+        | [] -> acc
+        //derivo anche i monomi seguenti
+        | x :: xs ->
+            //controllo il risultato della derivazione e lo concateno alla lista solo se diverso da 0
+            //e questo perche la derivata di un termine noto e 0
+            if deriva_singolo_monomio x = Monomial ( rational ( 0 , 1 ) , 0 ) then
+                aux xs acc
+            else aux xs ( acc @ [ deriva_singolo_monomio x ] )
+
+    //fa parte della funzione derive
+    in
+    match p with
+    //estraggo la lista di monomi
+    | Polynomial ( xs ) ->
+        //calcolo la derivata della lista di monomi appena ottenuti
+        Polynomial ( aux xs [] )
+
+//funzione che riduce l espressione nel caso questa sia composta da piu derivazioni o altro
+let reduce (e : expr) : polynomial = 
+
+    //fnzione ausiliare per poter derivare piu volte ricorsivamente
+    let rec aux (e : expr) : polynomial =
+        //estraggo le informazioni dall espressione e
+        match e with
+        //nel caso quello inserito sia un polinomio
+        | Poly xs -> xs
+        //nel caso quello inserito sia da derivare richiamo derive ricorsivamente
+        | Derive xs -> 
+            derive ( aux xs )
+
+    //fa parte della funzione reduce
+    in
+    aux e
+
+//funzione per unire le due parti di un uguaglianza
+let unisci_due_polinomi ( p1 : polynomial ) ( p2 :polynomial ) : polynomial =
+
+    //funzione ausiliaria
+    let rec aux ( p1 : monomial list ) ( p2 : monomial list ) ( acc : monomial list ) : monomial list =
+        match p1 , p2 with
+        //se sono nulli entrambi restituisco accumulatore
+        | [] , [] -> acc
+        //se una nulla resituisco l accumulatore piu l altra lista
+        | [] , _ -> 
+            //devo invertire i segni del secondo polinomio 
+            //estrapolo la lista di monomi
+            match ( polynomial_negate ( Polynomial ( p2 ) ) ) with
+            | Polynomial ( xs ) -> acc @ xs
+        | _ , [] -> acc @ p1
+        //se tutte e due valide concateno il primo elemento della prima lista
+        | x :: xs , y :: ys ->
+            aux xs p2 ( acc @ [ x ] )
+
+    //fa parte della funzione unisci_due_polinomi
+    in
+    match p1 , p2 with
+    //estrapolo dai due polinomi le loro liste di monomi e poi le passo alla funzione per concatenarle
+    | Polynomial ( xs ) , Polynomial ( ys ) ->
+        //concateno le due liste di monomi appena ottenuti
+        Polynomial ( aux xs ys [] )   
+
+//funzione per risolvere equazioni di grado 0 tramite identita se uguali a 0
+let solve0 (np : normalized_polynomial) : bool = 
+    //estraggo l array di rational che lo compone
+    match np with
+    | NormalizedPolynomial ( xs ) ->
+        //se il termine noto è uguale a 0 restituisco vero se no falso
+        if xs.[0] = rational ( 0 ) then true
+        else false
+
+//funzione per risolvere equazioni di grado 1 
+let solve1 (np : normalized_polynomial) : rational = 
+    //estraggo l array di rational che lo compone
+    match np with
+    | NormalizedPolynomial ( xs ) ->
+        //uso la formula da ax + b = 0 -> x = - b / a
+        - xs.[ 0 ] / xs.[ 1 ]
+
+//funzione per risolvere equazioni di grado 2
+let solve2 (np : normalized_polynomial) : (float * float option) option = 
+    //estraggo l array di rational che lo compone
+    match np with
+    | NormalizedPolynomial ( xs ) ->
+        //uso la formula da ax^2 + bx + c = 0 e uso la classica formula
+        //memorizzo il delta
+        let delta = xs.[ 1 ] ** 2 - rational ( 4 ) * xs.[ 2 ] * xs.[ 0 ]
+        //controllo il delta
+        //se positivo doppia soluzione
+        if delta > rational ( 0 ) then
+            //calcolo tutti i parametri della formula risolutiva e li converto in float dato il tipo restituito della funzione
+            let menob = ( float ) ( - xs.[ 1 ] )
+            let ballaseconda = ( float ) ( xs.[ 1 ] * xs.[ 1 ] )
+            let quattroac = ( float ) ( rational ( 4 ) * xs.[ 2 ] * xs.[ 0 ] )
+            let duea = ( float ) ( rational ( 2 ) * xs.[ 2 ] )
+            //memorizzo le due soluzioni
+            let sol1 = ( menob - sqrt ( ballaseconda - quattroac ) ) / duea
+            let sol2 = ( menob + sqrt ( ballaseconda - quattroac ) ) / duea
+            Some ( sol1 , Some ( sol2 ) )
+        //se uguale a zero due soluzioni coincidenti
+        else if delta = rational ( 0 ) then
+            //calcolo tutti i parametri della formula risolutiva e li converto in float dato il tipo restituito della funzione
+            let menob = ( float ) ( - xs.[ 1 ] )
+            let ballaseconda = ( float ) ( xs.[ 1 ] * xs.[ 1 ] )
+            let quattroac = ( float ) ( rational ( 4 ) * xs.[ 2 ] * xs.[ 0 ] )
+            let duea = ( float ) ( rational ( 2 ) * xs.[ 2 ] )
+            //memorizzo le due soluzioni
+            let sol1 = ( menob - sqrt ( ballaseconda - quattroac ) ) / duea
+            Some ( sol1 , None )
+        //se minore di 0 nessuna sluzione reale
+        else
+            None
+
+
+
+
---
+++ Main.fs
@@ -0,0 +1,295 @@
+﻿(*
+ * SharpSolver - Progetto di Programmazione e Calcolo a.a. 2018-19
+ * Main.fs: console e codice main
+ *)
+
+module SharpSolver.Main
+
+open Microsoft.FSharp.Text.Lexing
+open Absyn
+open SharpSolver.Impl
+open System
+open Prelude
+open Microsoft.FSharp.Text
+
+
+// funzioni di logging e printing
+//
+
+let hout hd fmt =
+    if not <| String.IsNullOrWhiteSpace hd then
+        printf "[%s]%s" hd (new String (' ', max 1 (Config.prefix_max_len - String.length hd)))
+        stdout.Flush ()
+    printfn fmt
+
+let chout col hd fmt =
+    let c = Console.ForegroundColor
+    Console.ForegroundColor <- col
+    Printf.kprintf (fun s -> hout hd "%s" s; Console.ForegroundColor <- c) fmt
+
+let out fmt = hout "" fmt
+let cout col fmt = chout col "" fmt
+
+let norm fmt = chout ConsoleColor.Yellow "norm" fmt
+let redux fmt = chout ConsoleColor.Magenta "redux" fmt
+let sol fmt = chout ConsoleColor.Green "sol" fmt
+let ident fmt = chout ConsoleColor.Green "ident" fmt    
+let error fmt = chout ConsoleColor.Red "error" fmt  
+//inserito questo per aver un format per il grado
+let degree fmt = chout ConsoleColor.Blue "degree" fmt
+
+
+// interprete dei comandi e delle espressioni
+//
+
+let interpreter_loop () =
+    while true do
+        printf "\n%s" Config.prompt_prefix          // stampa il prompt
+        stdout.Flush ()                             // per sicurezza flusha lo stdout per vedere la stampa del prompt senza end-of-line
+        let input = Console.ReadLine ()             // leggi l'input scritto dall'utente
+        let lexbuf = LexBuffer<_>.FromString input  // crea un lexbuffer sulla stringa di input
+
+        // funzione locale per il pretty-printing degli errori
+        let localized_error msg =
+            let tabs = new string (' ', Config.prompt_prefix.Length + lexbuf.StartPos.Column)
+            let cuts = new string ('^', let n = lexbuf.EndPos.Column - lexbuf.StartPos.Column in if n > 0 then n else 1)
+            cout ConsoleColor.Yellow "%s%s\n" tabs cuts
+            error "error at %d-%d: %s" lexbuf.StartPos.Column lexbuf.EndPos.Column msg 
+        
+        // blocco con trapping delle eccezioni
+        try
+            let line = Parser.line Lexer.tokenize lexbuf    // invoca il parser sul lexbuffer usando il lexer come tokenizzatore
+            #if DEBUG
+            hout "absyn" "%+A" line
+            hout "pretty" "%O" line
+            #endif
+
+            // interpreta la linea in base al valore di tipo line prodotto dal parsing
+            match line with
+            | Cmd "help" ->
+                out "%s" Config.help_text
+
+            | Cmd ("quit" | "exit") ->
+                out "%s" Config.exit_text
+                exit 0
+
+            // TODO: se volete supportare altri comandi, fatelo qui (opzionale)
+            
+            | Cmd s -> error "unknown command: %s" s    // i comandi non conosciuti cadono in questo caso
+
+            // TODO: aggiungere qui sotto i pattern per i casi Expr ed Equ con relativo codice per, rispettivamente, normalizzare espressioni e risolvere equazioni
+
+            //caso unica espressione senza uguale
+            | Expr e1 ->
+
+                //funzione che viene chiamata quando la mia espressione data in input è direttamente un polinomio e quindi non bisogna derivarla
+                let poly ( e1 : expr ) ( xs : polynomial ) : unit =
+                    //stampo la derivata che nel caso non venga passato la d[] viene stampato direttamente il polinomio dato in input
+                    redux "%O"  xs
+                    //stampo la normalizzazione
+                    norm "%O" ( normalize xs )
+                    //stampo il grado
+                    degree "%O" ( normalized_polynomial_degree ( normalize xs ) )
+
+                //funzione che viene chiamata quando la mia espressione data in input bisogna derivarla almeno una volta
+                let deri ( e1 : expr ) ( xs : expr ) : unit =
+                    //stampo la derivata ma prima la calcolo ricorsivamente
+                    redux "%O"  ( reduce e1 )
+                    //stampo la normalizzazione
+                    norm "%O" ( normalize ( reduce e1 ) )
+                    //stampo il grado
+                    degree "%O" ( normalized_polynomial_degree ( normalize ( reduce e1 ) ) )
+
+                //controllo che dati che contiene e1 se direttamente un polinomio o un espressione da derivare
+                match e1 with
+                //nel caso quello inserito sia un polinomio
+                | Poly xs ->
+                    poly e1 xs
+                //nel caso quello inserito sia da derivare
+                | Derive xs ->
+                    deri e1 xs
+
+            //caso uguaglianza con due espressioni
+            | Equ (e1, e2) ->
+
+                //funzione che dato un normalized_polynomial mi restituisce una monomial list e la ho fatta per potermi creare la mia equazione e usare il suo metodo tostring
+                let rational_to_monomial ( array : normalized_polynomial ) : monomial list =
+
+                    //funzione ausiliare
+                    let rec aux ( arr1 : rational [] ) ( lista : monomial list ) ( cont : int ) : monomial list =
+                        //se l array è vuoto resituisco la lista
+                        if Array.length arr1 = 0 then
+                            lista
+                        //escludo il primo elemento dell array e lo concateno alla lista e il contatore lo uso come posizione che indica il grado del singolo monomio che viene incrementato di volta in volta
+                        else aux ( Array.skip 1 arr1 ) ( lista @ [ Monomial ( arr1.[ 0 ] , cont ) ] ) ( cont + 1 )
+
+                    //fa parte della funzione rational_to_monomial
+                    in
+                    //estraggo l array di rational
+                    match array with
+                    | NormalizedPolynomial ( xs ) ->
+                        //mi creo il monomial [] e lo inizializzo
+                        aux xs [] 0
+
+                //funzione per stampare le soluzioni di un polinomio normalizzato
+                let stampa_soluzioni ( norma : normalized_polynomial ) ( grado : int ) : unit =
+                    //stampo le soluzioni
+                    //caso identità grado 0
+                    if grado = 0 then
+                        //la risolvo e se vero restituisco soltanto vero perchè uso identità
+                        if ( solve0 norma ) then
+                            ident "%s" "True" 
+                        else 
+                            //se no falso perche numero diverso da zero
+                            ident "%s" "False" 
+                    //1 soluzione grado 1
+                    else if grado = 1 then
+                        //la risolvo e stampo l eventuale soluzione tramite il format sol
+                        sol "%O" ( solve1 norma )
+                    //0,1,2 soluzioni grado 2
+                    else if grado = 2 then
+                        //la risolvo e guardo la quantita di soluzioni per vedere quante soluzioni sono reali e quante immaginarie
+                        match solve2 norma with
+                        //nel caso ci siano due soluzioni
+                        | Some ( x , Some ( y ) ) ->
+                            sol "%s" ( "x1 = " + (string) x + " e " + "x2 = " + (string) y )
+                        //nel caso ci sia un unica soluzione
+                        | Some ( x , None ) ->
+                            sol "%s" ( "x1 = " + (string) x + " e " + "x2 = soluzione complessa" )
+                        //nel caso non ci sia nessuna soluzione
+                        | None ->
+                            sol "%s" ( "Nessuna soluzione reale" )
+                    else 
+                        //nel caso il grado dell equazione e maggiore di 2
+                        sol "%s" "Superiore al grado 2" 
+
+                //funzione che racchiude il caso del match poly poly
+                let poly_poly ( e1 : expr ) ( e2 : expr ) ( xs : polynomial ) ( ys : polynomial ) : unit =
+                    
+                    //mi creo l equazione per poter usare il metodo tostring del tipo
+                    let eq1 = Equ ( e1 , e2 )
+                    //stampo la derivata
+                    redux "%O" eq1
+
+                    //mi creo l equazione per poter usare il metodo tostring del tipo
+                    let norma = normalize ( unisci_due_polinomi xs ys )
+                    let eq2 = Equ ( Poly ( Polynomial ( rational_to_monomial ( norma ) ) )  , Poly ( Polynomial ( [ Monomial ( rational ( 0 ) , 0 ) ] ) ) )
+                    //stampo la normalizzazione
+                    norm "%O" eq2
+                    
+                    //stampo il grado
+                    let grado = normalized_polynomial_degree ( normalize ( unisci_due_polinomi xs ys ) )
+                    degree "%d" grado
+
+                    //stampo le soluzioni
+                    stampa_soluzioni norma grado
+
+                //funzione che racchiude il caso del match poly deri
+                let poly_deri ( e1 : expr ) ( e2 : expr ) ( xs : polynomial ) ( ys : polynomial ) : unit =
+
+                    //mi creo l equazione per poter usare il metodo tostring del tipo
+                    let eq1 = Equ ( e1 , Poly ys )
+                    //stampo la derivata
+                    redux "%O" eq1
+
+                    //mi creo l equazione per poter usare il metodo tostring del tipo
+                    let norma = normalize ( unisci_due_polinomi xs ys )
+                    let eq2 = Equ ( Poly ( Polynomial ( rational_to_monomial ( norma ) ) )  , Poly ( Polynomial ( [ Monomial ( rational ( 0 ) , 0 ) ] ) ) )
+                    //stampo la normalizzazione
+                    norm "%O" eq2
+                    
+                    //stampo il grado
+                    let grado = normalized_polynomial_degree ( normalize ( unisci_due_polinomi xs ys ) )
+                    degree "%d" grado
+
+                    //stampo le soluzioni
+                    stampa_soluzioni norma grado
+                    
+                
+                //funzione che racchiude il caso del match deri poly
+                let deri_poly ( e1 : expr ) ( e2 : expr ) ( xs : polynomial ) ( ys : polynomial ) : unit =
+
+                    //mi creo l equazione per poter usare il metodo tostring del tipo
+                    let eq1 = Equ ( Poly ( xs ) , e2 )
+                    //stampo la derivata
+                    redux "%O" eq1
+
+                    //mi creo l equazione per poter usare il metodo tostring del tipo
+                    let norma = normalize ( unisci_due_polinomi xs ys )
+                    let eq2 = Equ ( Poly ( Polynomial ( rational_to_monomial ( norma ) ) )  , Poly ( Polynomial ( [ Monomial ( rational ( 0 ) , 0 ) ] ) ) )
+                    //stampo la normalizzazione
+                    norm "%O" eq2
+                    
+                    //stampo il grado
+                    let grado = normalized_polynomial_degree ( normalize ( unisci_due_polinomi xs ys ) )
+                    degree "%d" grado
+
+                    //stampo le soluzioni
+                    stampa_soluzioni norma grado
+                
+                //funzione che racchiude il caso del match poly poly
+                let deri_deri ( e1 : expr ) ( e2 : expr ) ( xs : polynomial ) ( ys : polynomial ) : unit =
+
+                    //mi creo l equazione per poter usare il metodo tostring del tipo
+                    let eq1 = Equ ( Poly ( xs ) , Poly ( ys ) )
+                    //stampo la derivata
+                    redux "%O" eq1
+
+                    //mi creo l equazione per poter usare il metodo tostring del tipo
+                    let norma = normalize ( unisci_due_polinomi xs ys )
+                    let eq2 = Equ ( Poly ( Polynomial ( rational_to_monomial ( norma ) ) )  , Poly ( Polynomial ( [ Monomial ( rational ( 0 ) , 0 ) ] ) ) )
+                    //stampo la normalizzazione
+                    norm "%O" eq2
+
+                    //stampo il grado
+                    let grado = normalized_polynomial_degree ( normalize ( unisci_due_polinomi xs ys ) )
+                    degree "%d" grado
+
+                    //stampo le soluzioni
+                    stampa_soluzioni norma grado
+
+                //controllo che dati contiene e1 e in caso derivo subito le espressioni per evitare operazioni identiche future
+                match e1 , e2 with
+                //nel caso tutte e due le parti siano polinomi
+                | Poly xs, Poly ys -> 
+                    poly_poly e1 e2 xs ys
+                //nel caso quello di destra sia da derivare e quello di sinistra è un polinomio 
+                | Poly xs , Derive ys ->
+                    poly_deri e1 e2 xs ( reduce e2 )
+                //nel caso quello di destra sia un polinomio e quello di sinistra sia da derivare
+                | Derive xs , Poly ys ->
+                    deri_poly e1 e2 ( reduce e1 ) ys
+                //nel caso da entrambe le parti bisogna derivare
+                | Derive xs , Derive ys -> 
+                    deri_deri e1 e2 ( reduce e1 ) ( reduce e2 )
+
+            | _ -> raise (NotImplementedException (sprintf "unknown command or expression: %O" line))
+                   
+        // gestione delle eccezioni
+        with LexYacc.ParseErrorContextException ctx ->
+                let ctx = ctx :?> Parsing.ParseErrorContext<Parser.token>
+                localized_error (sprintf "syntax error%s" (match ctx.CurrentToken with Some t -> sprintf " at token <%O>" t | None -> ""))
+
+           | Lexer.LexerError msg -> localized_error msg 
+
+           | :? NotImplementedException as e -> error "%O" e
+        
+           | e -> localized_error e.Message
+
+
+// funzione main: il programma comincia da qui
+//
+
+[<EntryPoint>]
+let main _ = 
+    let code =
+        try
+            interpreter_loop ()                 // chiama l'interprete
+            0                                   // ritorna il codice di errore 0 (nessun errore) al sistema operativo se tutto è andato liscio
+        with e -> error "fatal error: %O" e; 1  // in caso di eccezione esce con codice di errore 1
+    #if DEBUG
+    Console.ReadKey () |> ignore                // aspetta la pressione di un tasto prima di chiudere la finestra del terminare 
+    #endif
+    code
+
+
